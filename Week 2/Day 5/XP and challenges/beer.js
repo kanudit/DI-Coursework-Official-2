b063d06ed9f1711706a4650f40a8b723b181d27f
kanudit <gmartinwaxman@gmail.com>
2021-04-18 16:44:13 +0300

    beer

--- Week 2/Day 5/XP and challenges/beer.js
+++ Week 2/Day 5/XP and challenges/beer.js
@@ -1,11 +1,14 @@
+//how many bottle on the wall do you have, 99. 99 is the only acceptable number.
 let start = +prompt("how many bottles to get crunk?");
+//subtrahend is the number we subtract by
 let subtrahend = 1;
 
-
+//first line stanza of song
 console.log(start + " bottles of beer on the wall");
 console.log(start + " bottles of beer on the wall");
 console.log("Take " + subtrahend + " down, pass it around");
 
+//loop for the song
 let bottles = start - 1;
 // console.log(start + " bottles of beer on the wall")
 if (bottles > bottles - 1)
@@ -13,15 +16,12 @@ if (bottles > bottles - 1)
         console.log(bottles + " bottles of beer on the wall");
         console.log(bottles + " bottles of beer on the wall");
         console.log(bottles + " bottles of beer ");
+        //bottles on the wall and bottle coming down are not related
         bottles--;
         subtrahend++;
         console.log("Take " + subtrahend + " down, pass it around");
 
     }
-
+    //stop condition
     while (bottles > 0);
 console.log("walk around,cant hear a sound, now its time to lay on the ground");
-
-
-//if (bottles == 0) {
-// console.log("walk around,cant hear a sound, now its time to lay on the ground");
