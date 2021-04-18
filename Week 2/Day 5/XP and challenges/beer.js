a0255c9577b1e45829aeecd8b9d6feff881b4a11
kanudit <gmartinwaxman@gmail.com>
2021-04-18 16:37:42 +0300

    daily challeng

---
+++ Week 2/Day 5/XP and challenges/beer.js
@@ -0,0 +1,31 @@
+let start = +prompt("how many bottles to get crunk?");
+let subtrahend = 1;
+let oddEven = start % 2;
+
+console.log(start + " bottles of beer on the wall");
+console.log(start + " bottles of beer on the wall");
+console.log("Take " + subtrahend + " down, pass it around");
+
+let bottles = start - 1;
+// console.log(start + " bottles of beer on the wall")
+if (bottles > bottles - 1)
+    do {
+        console.log(bottles + " bottles of beer on the wall");
+        console.log(bottles + " bottles of beer on the wall");
+        console.log(bottles + " bottles of beer ");
+        bottles--;
+        subtrahend++;
+        console.log("Take " + subtrahend + " down, pass it around");
+
+    }
+
+    while (bottles > 0);
+console.log("walk around,cant hear a sound, now its time to lay on the ground");
+
+
+//if (bottles == 0) {
+// console.log("walk around,cant hear a sound, now its time to lay on the ground");
+
+
+console.log(oddEven);
+// for (i = start; i >= 0; subtrahend++) {
