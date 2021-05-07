95a8f49fad730f0156da3974693806ab73889255
kanudit <gmartinwaxman@gmail.com>
2021-05-08 01:41:59 +0300

    toop

---
+++ Week 5/Day 5/XP and challenges/measure_load.py
@@ -0,0 +1,13 @@
+import requests
+import time
+
+def load_time(url):
+    response = requests.get(url)
+    # print(f'{url} response)
+    print(f'time to load for: {url}\n{response.elapsed}')
+load_time('http://learn.di-learning.com/courses/')
+load_time('https://www.geeksforgeeks.org/response-elapsed-python-requests/')
+load_time('https://www.google.co.il/')
+
+
+# https://www.geeksforgeeks.org/response-elapsed-python-requests/
